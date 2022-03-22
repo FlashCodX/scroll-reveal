9a1c8f5aef2ed9306d3e35507a702118d4cec2bd
FlashCodX <luis.pinto.trading@gmail.com>
2022-03-22 16:55:42 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,288 @@
-import logo from './logo.svg';
-import './App.css';
+import Fade from "react-reveal/Fade";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fade top>
+        <h1>Reveal Elements On Scroll</h1>
+      </Fade>
+
+      <section className="s1">
+        <Fade left>
+          <img
+            src="https://images.unsplash.com/photo-1647795622128-aa8206f49948?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80"
+            alt=""
+          />
+        </Fade>
+        <Fade right>
+          <section>
+            <h1>Lorem, ipsum.</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Asperiores fuga at non, laboriosam alias laborum neque soluta
+              error sunt voluptatum iste nam, eum nulla culpa. Dolor voluptatem
+              ratione sunt, tenetur dolores, consequatur quibusdam sed
+              architecto autem quaerat saepe suscipit consectetur, provident
+              dignissimos cumque in labore sequi atque iure dolore voluptatibus!
+            </p>
+          </section>
+        </Fade>
+      </section>
+
+      <Fade bottom>
+        <div className="socials">
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-instagram"></i>
+          <i className="fa-brands fa-github"></i>
+          <i className="fa-brands fa-linkedin"></i>
+        </div>
+      </Fade>
+
+      <Fade>
+        <h1 className="fh">First Header</h1>
+      </Fade>
+      <section className="s2">
+        <Fade left>
+          <img
+            src="https://images.unsplash.com/photo-1580780965002-6ca357516eb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80"
+            alt=""
+          />
+        </Fade>
+        <Fade right>
+          <section>
+            <h1>Description</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam
+              repudiandae quod minus, rerum ipsam laborum nesciunt commodi dolor
+              eligendi recusandae eveniet minima? Ipsum minima rem enim aliquid
+              et magnam officiis itaque distinctio alias dicta? Ipsa, sunt!
+              Quos, laborum commodi! Vero!
+            </p>
+          </section>
+        </Fade>
+      </section>
+      <Fade left>
+        <h1 className="lp">Latest Posts</h1>
+      </Fade>
+
+      <section className="s3">
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1647705331414-a6318a8bbcca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1487424439918-bc6b5bef0380?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1581196607303-95c00f31c676?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1486496146582-9ffcd0b2b2b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+      </section>
+      <h1>Popular Posts</h1>
+
+      <section className="s4">
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1517677129300-07b130802f46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1438109491414-7198515b166b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1507101105822-7472b28e22ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1581196607303-95c00f31c676?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1486496146582-9ffcd0b2b2b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+          />
+          <section>
+            <h1>I am the title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              voluptates, aliquid ab nesciunt autem assumenda, iusto,
+              repudiandae sunt a vitae id consequatur eaque? Quia ad voluptas
+              minima officia consequatur rerum quod, accusamus delectus nemo in
+              ea reprehenderit error quaerat iusto, id iure fugiat ipsa soluta
+              est nesciunt enim architecto praesentium?
+            </p>
+          </section>
+        </article>
+      </section>
     </div>
   );
 }
